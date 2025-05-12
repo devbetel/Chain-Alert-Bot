0bcbf6c9aca5d40af83847edacdc425ae58147b3
Lucas Betel <97856858+devbetel@users.noreply.github.com>
2025-05-12 16:20:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from config.settings import dp, bot
 from handlers import commands
 import logging
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
-from aiogram.dispatcher.webhook import get_new_configured_app
-from aiogram.types import BotCommand
 import os
 from utils.middleware import LoggingMiddleware
 
@@ -11,27 +10,31 @@ from utils.middleware import LoggingMiddleware
 logging.basicConfig(level=logging.DEBUG)
 dp.message.middleware(LoggingMiddleware())
 
-# Configura o webhook
-async def on_startup(app):
-    webhook_url = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/webhook"
-    await bot.set_webhook(webhook_url)
-    await set_bot_commands()
-    logging.info(f"Webhook configurado em: {webhook_url}")
+# Configurações do Webhook
+WEBHOOK_PATH = f"/webhook"
+WEBHOOK_URL = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}{WEBHOOK_PATH}"
 
-# Inicializa o aplicativo webhook
-app = get_new_configured_app(dp, path="/webhook", bot=bot)
-app.on_startup.append(on_startup)
+# Cria o aplicativo aiohttp
+app = web.Application()
+dp.include_router(commands.router)
+
+# Configura o webhook no aplicativo aiohttp
+webhook_requests_handler = SimpleRequestHandler(
+    dispatcher=dp,
+    bot=bot,
+)
+webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+setup_application(app, dp, bot=bot)
 
 # Ponto de entrada para o Render
 if __name__ == "__main__":
-    dp.include_router(commands.router)  # Inclui os roteadores
-
-    # Se estiver em desenvolvimento local, use polling (para testes)
     if os.getenv("ENV") == "dev":
         logging.info("Modo: Polling (local)")
-        from aiogram import executor
-        executor.start_polling(dp, skip_updates=True)
+        import asyncio
+        async def polling():
+            await bot.delete_webhook()
+            await dp.start_polling(bot)
+        asyncio.run(polling())
     else:
-        # Modo produção (Render)
         logging.info("Modo: Webhook (Render)")
         web.run_app(app, host="0.0.0.0", port=10000)
